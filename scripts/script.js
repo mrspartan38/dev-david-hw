ab72fda1c1d42c4c7b39304e642d98af0e0f2aa0
Evan S. <mrspartan38@gmail.com>
2024-10-03 14:53:57 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,35 @@
 $(document).ready(function(){
   
-    $('#reset').on('click', function(){
-      //move my doge back to the start 
-      $("#dogePic").css( 'top', '0px').css('left', '0px');
-      //https://jqueryui.com/position/
-    })
-    
-    $("img").draggable( {
-        stop: function() {
-         // alert('dragged'); 
-          
-          if ( $("img").position().left > 100)
-            {
-              //they won
-              $("body").append("<h2>YOU WON</h2> ") 
-            }
-          // alert( $("img").position().left ); 
-          
-   
-        }
-    });  
-   
-    //TODO: if position is on the far rihgt side, tell user they won 
-    
-    
-   
-    
-    
-    
-  });
+  $('#reset').on('click', function(){
+    //move my doge back to the start 
+    $("#sillyCat").css( 'top', '0px').css('left', '0px');
+    const element = document.getElementById("h2");
+    element.remove();
+    //https://jqueryui.com/position/
+  })
+  
+  $("img").draggable( {
+      stop: function() {
+       // alert('dragged'); 
+        
+        if ( $("img").position().left > 100)
+          {
+            $("body").append("<h2>YOU WON</h2> ") 
+          }
+        else (
+          $("body").append("<h2>keep searching</h2> ")
+        )
+        // alert( $("img").position().left ); 
+        
+ 
+      }
+  });  
+ 
+  //TODO: if position is on the far rihgt side, tell user they won 
   
   
+ 
+  
+  
+  
+});
